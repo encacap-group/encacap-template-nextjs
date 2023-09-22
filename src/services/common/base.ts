f484fcc9be9b4b2fee914aa4dd68ae3184194e82
Nguyễn Khắc Khánh <encacap@gmail.com>
2023-09-22 06:13:39 +0000

    fix: resolve sentry example linting issues

--- src/services/common/base.ts
+++ src/services/common/base.ts
@@ -1,8 +1,6 @@
 import { cache } from 'react';
 
 export const getHomeData = cache(async () => {
-  console.log('getHomeData');
-
   return Promise.resolve({
     title: 'Home',
     content: 'Hello World!',
